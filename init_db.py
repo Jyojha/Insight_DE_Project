f855e6de7d9a7e1994f5b725a5dc531d2b199996
JJha <jyots.jha@gmail.com>
2017-06-07 13:17:22 -0700

    handle bidirectional streets properly

--- init_db.py
+++ init_db.py
@@ -6,7 +6,7 @@ import settings
 # function to convert each row item in dictionary
 def df_to_dict(df):
     return df.to_dict(orient='records')
-    
+
 def extract_intersections(edges_list):
     intersection_dict = {}
 
@@ -17,7 +17,7 @@ def extract_intersections(edges_list):
 
         if item['to_cnn'] not in intersection_dict:
             intersection_dict[item['to_cnn']] = item['to_cnn_coords']
-    
+
     intersections = []
 
     for key, value in intersection_dict.iteritems():
@@ -27,27 +27,51 @@ def extract_intersections(edges_list):
         intersections.append(tmp_dict)
 
     return intersections
-            
 
+def massage_edges(edges):
+    massaged = []
 
+    for edge in edges:
+        cleaned = {'cnn': edge['cnn'],
+                   'streetname': edge['streetname'],
+                   'classcode': edge['classcode'],
+                   'centerline': edge['centerline'],
+                   'length': edge['length']}
 
+        t, f = edge["to_cnn"], edge["from_cnn"]
+
+        if edge['oneway'] in ['B', 'T']:
+            cleaned['from_cnn'] = t
+            cleaned['to_cnn']   = f
+
+            massaged.append(cleaned.copy())
+
+        if edge['oneway'] in ['B', 'F']:
+            cleaned['from_cnn'] = f
+            cleaned['to_cnn']   = t
+
+            massaged.append(cleaned)
+
+    return massaged
 
 def main():
-    ''' Populate the static road network data in to the database'''
-    
+    '''Populate the static road network data in to the database'''
+
     # Read the data
     edges = pd.read_pickle(settings.EDGE_PATH)
     node_neighbors = pd.read_json(settings.NODE_NEIGHBORS_PATH, orient='records')
-    
+
     # Define the entry point to the database
-    driver = GraphDatabase.driver(settings.DB_URL, 
+    driver = GraphDatabase.driver(settings.DB_URL,
                                   auth=basic_auth(settings.USER_NAME, settings.USER_PASSWORD))
     db = driver.session()
 
     db.run("MATCH (n) DETACH DELETE n")
-    
+
     edges_list = df_to_dict(edges)
     nodes = extract_intersections(edges_list)
+
+    massaged_edges = massage_edges(edges_list)
 
     r = db.run('''UNWIND {nodes} as node
                   CREATE (n:Intersection)
@@ -61,8 +85,10 @@ def main():
     r = db.run('''UNWIND {edges} as edge
                   MATCH (f:Intersection {cnn: edge.from_cnn}),
                         (t:Intersection {cnn: edge.to_cnn})
-                  CREATE (f)-[s:Segment {cnn: edge.cnn, from_cnn: edge.from_cnn, to_cnn: edge.to_cnn, from_coordinates: edge.from_cnn_coords, to_coordinates: edge.to_cnn_coords, street: edge.streetname, classcode: edge.classcode, length: edge.length}]->(t)
-               ''', edges=edges_list)
+                  CREATE (f)-[s:Segment {cnn: edge.cnn, street: edge.streetname,
+                                         classcode: edge.classcode,
+                                         length: edge.length}]->(t)
+               ''', edges=massaged_edges)
 
     print r.summary()
 
@@ -70,5 +96,3 @@ def main():
 
 if __name__ == '__main__':
         main()
-
-        
