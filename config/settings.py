fa7df2c5db2ddd61a3c3b41410a3199ffd1d9d5b
JJha <jyots.jha@gmail.com>
2017-06-07 13:17:38 -0700

    absolute paths in settings.py

--- config/settings.py
+++ config/settings.py
@@ -1,12 +1,15 @@
-import os.path
+from os import path
 
-EDGE_PATH = "./street_data/street_segments.pickle"
-NODE_NEIGHBORS_PATH = "./street_data/node_neighbors.json"
+CONFIG_DIR = path.abspath(path.dirname(__file__))
+ROOT_DIR = path.abspath(path.join(CONFIG_DIR, '..'))
+
+EDGE_PATH = path.join(ROOT_DIR, "street_data/street_segments.pickle")
+NODE_NEIGHBORS_PATH = path.join(ROOT_DIR, "street_data/node_neighbors.json")
 DB_URL = 'bolt://localhost'
 USER_NAME = ""
 USER_PASSWORD = ""
 
 try:
-	from config.settings_local import *
+    from config.settings_local import *
 except ImportError:
-	pass
+    pass
