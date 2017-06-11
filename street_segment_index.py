Fix the computation of point to segment distance.

street_segment_index.py
@@ -1,4 +1,4 @@
-from numpy import array, cross
+from numpy import array, cross, dot
 from numpy.linalg import norm
 from scipy.spatial.distance import euclidean
 
@@ -15,8 +15,23 @@ class StreetSegment(object):
         self.cnn = cnn
         self.name = name
 
-class StreetSegmentIndex(object):
+    def __repr__(self):
+        return 'StreetSegment(%d, "%s")' % (self.cnn, self.name)
 
+class IndexItem(object):
+    def __init__(self, src, dst, src_xyz, dst_xyz, obj):
+        self.src = src
+        self.dst = dst
+        self.src_xyz = src_xyz
+        self.dst_xyz = dst_xyz
+        self.obj = obj
+
+    def __repr__(self):
+        return 'IndexItem%s' % str((self.src, self.dst,
+                                    self.src_xyz, self.dst_xyz,
+                                    self.obj))
+
+class StreetSegmentIndex(object):
     def __init__(self):
         props = Property()
         props.dimension = 3
@@ -42,9 +57,31 @@ class StreetSegmentIndex(object):
 
         return (minx, maxx, miny, maxy, minz, maxz)
 
-    def _point_line_distance(self, point, s1, s2):
+    def _bbox_for_point(self, xyz, radius):
+        x, y, z = xyz
+
+        return (x - radius, x + radius,
+                y - radius, y + radius,
+                z - radius, z + radius)
+
+    def _point_segment_distance(self, point, s1, s2):
         point = array(point)
 
+        # If the dot product is less than 0, this means that the perpendicular
+        # from the point lies outside the segment (the angle at s1 is greater
+        # than 90 degrees). So we use the distance to the corresponding end of
+        # the segment instead.
+        dot1 = dot(point - s1, s2 - s1)
+        if dot1 < 0:
+            return norm(point - s1)
+
+        # Same as above, just for the other end.
+        dot2 = dot(point - s2, s1 - s2)
+        if dot2 < 0:
+            return norm(point - s2)
+
+        # Otherwise, the shortest distance is the length of perpendicular to
+        # the segment line.
         return norm(cross(s1 - point, s2 - point)) / norm(s2 - s1)
 
     def add_street_segment(self, id, obj, segment):
@@ -53,26 +90,24 @@ class StreetSegmentIndex(object):
             xyz2 = self._toXYZ(node2)
             bbox = self._bbox(xyz1, xyz2)
 
-            self._rtree.insert(id, bbox, obj=(array(xyz1), array(xyz2), obj))
+            item = IndexItem(node1, node2, array(xyz1), array(xyz2), obj)
+            self._rtree.insert(id, bbox, obj=item)
 
-    def nearest_segments(self, lon, lat, radius=20, num=2):
-        x, y, z = self._toXYZ((lon, lat))
-
-        bbox = (x - radius, x + radius,
-                y - radius, y + radius,
-                z - radius, z + radius)
+    def nearest_segments(self, lon, lat, radius=10, num=2):
+        xyz = self._toXYZ((lon, lat))
+        bbox = self._bbox_for_point(xyz, radius)
 
         nearest_segments_list = []
         for item in self._rtree.intersection(bbox, objects=True):
-
-            tmp_dist = self._point_line_distance((x, y, z), item.object[0], item.object[1])
-
-            if tmp_dist <= radius:
-                nearest_segments_list.append((tmp_dist, item.id, item.object[2]))
+            dist = self._point_segment_distance(xyz,
+                                                item.object.src_xyz,
+                                                item.object.dst_xyz)
+            if dist <= radius:
+                nearest_segments_list.append((dist, item.id, item.object))
 
         nearest_segments_list.sort(key=lambda x: x[0])
 
-        return nearest_segments_list[0:num]
+        return nearest_segments_list[:num]
 
     def import_from_file(self, path=settings.SEGMENT_PATH):
         segments = read_segments(path)