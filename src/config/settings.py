8351c1608787a5c1bf5afbd2fc850420e7d4697a
JJha <jyots.jha@gmail.com>
2017-06-15 16:46:01 -0700

    Add settings module with ec2 specific settings.

--- src/config/settings.py
+++ src/config/settings.py
@@ -1,5 +1,5 @@
 import logging
-from os import path
+from os import path, environ
 
 SPARK_APP_NAME='traffic-processing'
 
@@ -14,6 +14,10 @@ KAFKA_TOPIC='traffic-events'
 STREET_INDEX_PICKLE = 'data/street_segment_index.pickle'
 
 LOGGING_LEVEL = logging.DEBUG
+
+SETTINGS_MODULE=environ.get('TRAFFIC_SETTINGS_MODULE')
+if SETTINGS_MODULE is not None:
+    exec("from config.settings_%s import *" % SETTINGS_MODULE)
 
 try:
     from config.settings_local import *
---
+++ src/config/settings_ec2.py
@@ -0,0 +1,1 @@
+KAFKA_URL='ec2-52-89-25-183.us-west-2.compute.amazonaws.com:9092'
