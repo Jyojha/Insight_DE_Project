8bf95aa01cb4fc014c6bf067ecca8cd82e708171
JJha <jyots.jha@gmail.com>
2017-06-16 03:38:38 -0700

    Use standard function to convert degrees to radians.

--- src/distance.py
+++ src/distance.py
@@ -1,6 +1,6 @@
 # For details see http://www.movable-type.co.uk/scripts/gis-faq-5.1.html
 
-from math import asin, sin, cos, sqrt, pi
+from math import asin, sin, cos, sqrt, radians
 
 from config import settings
 
@@ -15,14 +15,11 @@ lonlat2 = (-122.4453449903129, 37.77021017487183)
 # in meters
 EARTH_RADIUS = 6367000
 
-def deg2rad(d):
-    return pi * d / 180
-
 def haversin(x, y, R=EARTH_RADIUS):
     lon1, lat1 = x[0], x[1]
     lon2, lat2 = y[0], y[1]
 
-    lon1, lat1, lon2, lat2 = map(deg2rad, (lon1, lat1, lon2, lat2))
+    lon1, lat1, lon2, lat2 = map(radians, (lon1, lat1, lon2, lat2))
 
     dlon = lon2 - lon1
     dlat = lat2 - lat1
