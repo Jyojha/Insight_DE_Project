e7f1a86eab445c41a0aa295e3aa2a00722f0ecc9
JJha <jyots.jha@gmail.com>
2017-06-16 09:21:00 -0700

    Add neo4j url to ec2 profile.

--- src/config/settings_ec2.py
+++ src/config/settings_ec2.py
@@ -1,1 +1,3 @@
-KAFKA_URL='ec2-52-89-25-183.us-west-2.compute.amazonaws.com:9092'
+KAFKA_URL ='ec2-52-89-25-183.us-west-2.compute.amazonaws.com:9092'
+
+DB_URL = 'bolt://ec2-34-212-42-137.us-west-2.compute.amazonaws.com'
