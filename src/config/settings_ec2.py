bd54a26dd94b2651469a5d2b9bf863a57761fdd4
JJha <jyots.jha@gmail.com>
2017-06-19 15:29:12 -0700

    Reindent ALLOWED_HOSTS.

--- src/config/settings_ec2.py
+++ src/config/settings_ec2.py
@@ -3,5 +3,5 @@ KAFKA_URL ='ec2-52-89-25-183.us-west-2.compute.amazonaws.com:9092'
 DB_URL = 'bolt://ec2-34-212-42-137.us-west-2.compute.amazonaws.com'
 
 ALLOWED_HOSTS = ['ec2-34-212-42-137.us-west-2.compute.amazonaws.com',
-                'ec2-34-208-212-230.us-west-2.compute.amazonaws.com',
-                'ec2-34-212-66-191.us-west-2.compute.amazonaws.com']
+                 'ec2-34-208-212-230.us-west-2.compute.amazonaws.com',
+                 'ec2-34-212-66-191.us-west-2.compute.amazonaws.com']
